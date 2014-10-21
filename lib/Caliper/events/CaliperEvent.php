4d4587b5cc6b33f02fd739914d9f02cfa22b6241
Prashant Nayak <prashant@intellifylearning.com>
2014-10-21 15:05:20 -0400

    Update CaliperEvent with duration attribute.  Also created enums to capture JSON-LD context and type for events across all v1 metric profiles

--- lib/Caliper/events/CaliperEvent.php
+++ lib/Caliper/events/CaliperEvent.php
@@ -14,71 +14,129 @@ class CaliperEvent implements JsonSerializable {
     private $object;
     private $target;
     private $startedAt;
-    private $endedAt =0; 
+    private $endedAt = 0;
     private $edApp;
     private $lisOrganization;
     private $generated;
-    
+    private $duration;
+
     /*
     private $agent;
     private $activityContext;
     private $learningContext;
      */
-    
+
 
     /**
      * Create a new CaliperEvent
      */
     public function __construct() {
     }
-	
-	
-	/*
-	 * 
-	 */
-	public function __destruct() {
-    }
-    
-    /**
-     * @param mixed $context
+
+
+    /*
+     *
      */
-    public function setContext($context) {
-    	$this->context = $context;
+    public function __destruct() {
     }
-    
+
+    /**
+     *
+     * @see JsonSerializable::jsonSerialize()
+     * to implement jsonLD
+     */
+    public function jsonSerialize() {
+
+        return ['@context' => $this->getContext(),
+            '@type' => $this->getType(),
+            'edApp' => $this->getEdApp(),
+            'group' => $this->getLisOrganization(),
+            'actor' => $this->getActor(),
+            'action' => $this->getAction(),
+            'object' => $this->getObject(),
+            'target' => $this->getTarget(),
+            'generated' => $this->getGenerated(),
+            'startedAtTime' => $this->getStartedAt(),
+            'endedAtTime' => $this->getEndedAt(),
+            'duration' => $this->getDuration()
+        ];
+    }
+
     /**
      * @return mixed
      */
     public function getContext() {
-    	return $this->context;
+        return $this->context;
     }
+
     /**
-     * @param mixed $type
+     * @param mixed $context
      */
-    public function setType($type) {
-    	$this->type = $type;
+    public function setContext($context) {
+        $this->context = $context;
     }
-    
+
     /**
      * @return mixed
      */
     public function getType() {
-    	return $this->type;
+        return $this->type;
     }
-    
-    
+
     /**
-     * @param mixed $actor
+     * @param mixed $type
      */
-    public function setActor($actor) {
-    	$this->actor = $actor;
+    public function setType($type) {
+        $this->type = $type;
     }
-    
+
+    /**
+     * @return mixed
+     */
+    public function getEdApp() {
+        return $this->edApp;
+    }
+
+    /**
+     * @param mixed $edApp
+     */
+    public function setEdApp($edApp) {
+        $this->edApp = $edApp;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLisOrganization() {
+        return $this->lisOrganization;
+    }
+
+    /**
+     * @param mixed $lisOrganization
+     */
+    public function setLisOrganization($lisOrganization) {
+        $this->lisOrganization = $lisOrganization;
+    }
+
     /**
      * @return mixed
      */
     public function getActor() {
-    	return $this->actor;
+        return $this->actor;
+    }
+
+    /**
+     * @param mixed $actor
+     */
+    public function setActor($actor) {
+        $this->actor = $actor;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAction() {
+        return $this->action;
     }
 
     /**
@@ -91,60 +149,44 @@ class CaliperEvent implements JsonSerializable {
     /**
      * @return mixed
      */
-    public function getAction() {
-        return $this->action;
+    public function getObject() {
+        return $this->object;
     }
-    
+
     /**
      * @param mixed $object
      */
     public function setObject($object) {
-    	$this->object = $object;
-    }
-    
-    /**
-     * @return mixed
-     */
-    public function getObject() {
-    	return $this->object;
-    }
-    
-    /**
-     * @param mixed $edApp
-     */
-    public function setEdApp($edApp) {
-    	$this->edApp = $edApp;
-    }
-    
-    /**
-     * @return mixed
-     */
-    public function getEdApp() {
-    	return $this->edApp;
-    }
-    
-   /** 
-    * @param mixed $lisOrganization
-    */
-    public function setLisOrganization($lisOrganization) {
-    	$this->lisOrganization = $lisOrganization;
-    }
-    
-    /**
-     * @return mixed
-     */
-    public function getLisOrganization() {
-    	return $this->lisOrganization;
+        $this->object = $object;
     }
 
-   
-   
+    /**
+     *
+     */
+    public function getTarget() {
+        return $this->target;
+    }
 
     /**
-     * @param mixed $startedAtTime
+     * @param mixed $target
      */
-    public function setStartedAt($startedAt) {
-        $this->startedAt = $startedAt;
+    public function setTarget($target) {
+        $this->target = $target;
+    }
+
+    /**
+     * @return the generated
+     */
+    public function  getGenerated() {
+        return $this->generated;
+    }
+
+    /**
+     * @param generated
+     *            the generated to set
+     */
+    public function  setGenerated($generated) {
+        $this->generated = $generated;
     }
 
     /**
@@ -157,67 +199,35 @@ class CaliperEvent implements JsonSerializable {
     /**
      * @param mixed $startedAtTime
      */
-    public function setEndedAt($EndedAt) {
-    	$this->endedAt = $EndedAt;
+    public function setStartedAt($startedAt) {
+        $this->startedAt = $startedAt;
     }
-    
+
     /**
      * @return mixed
      */
     public function getEndedAt() {
-    	return $this->endedAt;
+        return $this->endedAt;
     }
-    
-    /**
-     * @param mixed $target
-     */
-   public function setTarget($target)
-   {
-   	$this->target = $target;
-   }
-   /**
-    * 
-    */
-   public function getTarget()
-   {
-   	return $this->target;
-   }
-   
 
-   /**
-    * @return the generated
-    */
-   public function  getGenerated() {
-   	return $this->generated;
-   }
-   
-   
-   /**
-    * @param generated
-    *            the generated to set
-    */
-   public function  setGenerated($generated) {
-   	$this->generated = $generated;
-   }
-  
     /**
-     * 
-     * @see JsonSerializable::jsonSerialize()
-     * to implement jsonLD
+     * @param mixed $startedAtTime
      */
-    public  function jsonSerialize(){
-    	
-    return ['@context'=>$this->getContext(),
-    		'@type'=>$this->getType(),
-    		'actor'=>$this->getActor(),
-			'action'=>$this->getAction(),
-			'object'=>$this->getObject(),
-			'target'=>$this->getTarget(),
-			'generated'=>$this->getGenerated(),
-			'startedAtTime'=>$this->getStartedAt(),
-			'endedAtTime'=>$this->getEndedAt(),
-    		'edApp'=>$this->getEdApp(),
-    		'group'=>$this->getLisOrganization()
-			];    
+    public function setEndedAt($EndedAt) {
+        $this->endedAt = $EndedAt;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDuration() {
+        return $this->duration;
+    }
+
+    /**
+     * @param mixed $duration
+     */
+    public function setDuration($duration) {
+        $this->duration = $duration;
     }
 }
---
+++ lib/Caliper/events/CaliperEventContexts.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: pnayak
+ * Date: 10/21/14
+ * Time: 1:28 PM
+ */
+class CaliperEventContexts extends SplEnum {
+
+    const
+        __default = "",
+        ANNOTATION = "http://purl.imsglobal.org/ctx/caliper/v1/AnnotationEvent",
+        ASSESSMENT = "http://purl.imsglobal.org/ctx/caliper/v1/AssessmentEvent",
+        ASSESSMENT_ITEM = "http://purl.imsglobal.org/ctx/caliper/v1/AssessmentItemEvent",
+        ASSIGNABLE = "http://purl.imsglobal.org/ctx/caliper/v1/AssignableEvent",
+        CALIPER_EVENT = "http://purl.imsglobal.org/ctx/caliper/v1/CaliperEvent",
+        MEDIA = "http://purl.imsglobal.org/ctx/caliper/v1/MediaEvent",
+        NAVIGATION = "http://purl.imsglobal.org/ctx/caliper/v1/NavigationEvent",
+        OUTCOME = "http://purl.imsglobal.org/ctx/caliper/v1/OutcomeEvent",
+        VIEWED = "http://purl.imsglobal.org/ctx/caliper/v1/ViewedEvent";
+}
+
