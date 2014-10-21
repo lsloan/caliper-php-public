0e86ef8733cc8fa3a65f99a6af5519a448fc07be
Prashant Nayak <prashant@intellifylearning.com>
2014-10-21 15:19:08 -0400

    first pass of updates to AnnotationEvents

--- lib/Caliper/events/annotation/AnnotationEvent.php
+++ lib/Caliper/events/annotation/AnnotationEvent.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once dirname(__FILE__).'/../CaliperEvent.php';
+require_once (dirname(dirname(dirname(__FILE__))).'/actions/AnnotationActions.php');
 
 /**
  * @author balachandiran.v
@@ -9,14 +10,19 @@ require_once dirname(__FILE__).'/../CaliperEvent.php';
 class AnnotationEvent extends CaliperEvent {
 	
 	public function __construct(){
-			$this->setContext("http://purl.imsglobal.org/ctx/caliper/v1/AnnotationEvent");
-			$this->setType("http://purl.imsglobal.org/caliper/v1/AnnotationEvent");
-	
+
+        $this->setContext(CaliperEventContexts::ANNOTATION);
+        $this->setType(CaliperEventTypes::ANNOTATION);
+
 	}
 
 	public static function forAction($action) {
+
 		$event = new AnnotationEvent();
+
+        // Add check to see if $action exists in AnnotationActions enum
 		$event->setAction($action);
+        
 		return $event;
 	}
 }
