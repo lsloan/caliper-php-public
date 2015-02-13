64b2f395312b0b60f7c08973213535b578a52d2e
Lance E Sloan <lsloan-github.com@umich.edu>
2015-02-13 12:00:56 -0500

    Removed author/license comments.
    Requested by @arwhyte

--- lib/Caliper/events/CaliperEvent.php
+++ lib/Caliper/events/CaliperEvent.php
@@ -1,12 +1,5 @@
 <?php
-
-/**
- *  author: Prashant Nayak
- *  ©2013 IMS Global Learning Consortium, Inc.  All Rights Reserved.
- *  For license information contact, info@imsglobal.org
- */
 class CaliperEvent implements JsonSerializable {
-
     private $context;
     private $type;
     private $actor;
