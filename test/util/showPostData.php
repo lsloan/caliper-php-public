912eda69b954fcab1c1d542a2e2a28ec27cfcf34
lsloan <lsloan@umich.edu>
2015-03-23 13:23:15 -0400

    IMSGlobal/caliper-php#30 - Reorganized sample apps and tools into new examples directory.  (Part 2, I guess.)

--- test/util/showPostData.php
+++
@@ -1,4 +0,0 @@
-<?php
-$rawPostData = file_get_contents('php://input');
-error_log("raw post data:\n" . $rawPostData);
-error_log('Received ' . strlen($rawPostData) . ' bytes');
--- test/util/testListener.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh --
-
-port=$1
-[ -z "$port" ] && port='8000'
-
-php -S localhost:${port} $(dirname $0)/showPostData.php
