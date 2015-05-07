2e796b7e85653a47998d989852c2ec88340a471f
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2015-05-07 13:10:35 -0400

    IMSGlobal/caliper-php#32 - Use BasicEnum.

--- lib/Caliper/entities/CaliperDigitalResourceTypes.php
+++ lib/Caliper/entities/CaliperDigitalResourceTypes.php
@@ -1,20 +1,15 @@
 <?php
+require_once 'util/BasicEnum.php';
 
-/**
- * Created by PhpStorm.
- * User: pnayak
- * Date: 10/21/14
- * Time: 1:28 PM
- */
-class CaliperDigitalResourceTypes extends SplEnum {
+class CaliperDigitalResourceTypes extends BasicEnum {
     const
-        __default = "",
-        CALIPER_ASSIGNABLE_DIGITAL_RESOURCE = "http://purl.imsglobal.org/caliper/v1/CaliperAssignableDigitalResource",
-        CALIPER_MEDIA_OBJECT = "http://purl.imsglobal.org/caliper/v1/CaliperMediaObject",
-        EPUB_CHAPTER = "http://www.idpf.org/epub/vocab/structure/#chapter",
-        EPUB_PART = "http://www.idpf.org/epub/vocab/structure/#part",
-        EPUB_SUB_CHAPTER = "http://www.idpf.org/epub/vocab/structure/#subchapter",
-        EPUB_VOLUME = "http://www.idpf.org/epub/vocab/structure/#volume",
-        WEB_PAGE = "http://purl.imsglobal.org/caliper/v1/WebPage";
+        __default = '',
+        CALIPER_ASSIGNABLE_DIGITAL_RESOURCE = 'http://purl.imsglobal.org/caliper/v1/CaliperAssignableDigitalResource',
+        CALIPER_MEDIA_OBJECT = 'http://purl.imsglobal.org/caliper/v1/CaliperMediaObject',
+        EPUB_CHAPTER = 'http://www.idpf.org/epub/vocab/structure/#chapter',
+        EPUB_PART = 'http://www.idpf.org/epub/vocab/structure/#part',
+        EPUB_SUB_CHAPTER = 'http://www.idpf.org/epub/vocab/structure/#subchapter',
+        EPUB_VOLUME = 'http://www.idpf.org/epub/vocab/structure/#volume',
+        WEB_PAGE = 'http://purl.imsglobal.org/caliper/v1/WebPage';
 }
 
