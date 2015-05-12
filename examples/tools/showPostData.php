0ccbca206fd8bfd0a4a0e5996a5ee7e69d1ebb39
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2015-05-12 15:12:23 -0400

    IMSGlobal/caliper-php#44 - Since apiKey is now sent in the "Authorization" header, update this example tool to show it and other interesting headers.

--- examples/tools/showPostData.php
+++ examples/tools/showPostData.php
@@ -1,4 +1,8 @@
 <?php
+$headers = getallheaders();
 $rawPostData = file_get_contents('php://input');
-error_log("raw post data:\n" . $rawPostData);
+
+error_log('Authorization: ' . $headers['Authorization']);
+error_log('Content-Type: ' . $headers['Content-Type']);
+error_log("Raw post data:\n" . $rawPostData);
 error_log('Received ' . strlen($rawPostData) . ' bytes');
