55c67707f0291a609ca4de0fdcab709a74a39a8c
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2015-05-12 13:14:20 -0400

    IMSGlobal/caliper-php#44 - New context class for envelopes.

---
+++ lib/Caliper/request/EnvelopeContext.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'util/BasicEnum.php';
+
+class EnvelopeContext extends \BasicEnum {
+
+    const
+        __default = '',
+        CONTEXT = 'http://purl.imsglobal.org/caliper/ctx/v1/Envelope';
+}
+
