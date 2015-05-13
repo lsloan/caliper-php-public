f60ca8b1940646c407d7a0ba18f6193984bbd7f5
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2015-05-13 11:58:04 -0400

    IMSGlobal/caliper-php#48 - Changed all setters to return instance of class, for fluent interface.  Reorganized methods, corrected indentation. Updated some incorrect PHPDoc.  Removed remaining author credits.

--- lib/Caliper/entities/CaliperAgent.php
+++ lib/Caliper/entities/CaliperAgent.php
@@ -1,13 +1,7 @@
 <?php
+require_once dirname(__FILE__) . '/foaf/Agent.php';
 
-require_once dirname(__FILE__).'/foaf/Agent.php';
-
-/**
- *@author balachandiran.v
- *
- */
 class CaliperAgent extends CaliperEntity implements Agent, JsonSerializable {
-
     function __construct() {
         parent::__construct();
     }
--- lib/Caliper/entities/annotation/SharedAnnotation.php
+++ lib/Caliper/entities/annotation/SharedAnnotation.php
@@ -1,43 +1,41 @@
 <?php
-
 require_once 'Annotation.php';
 
-/**
- * @author balachandiran.v
- *
- */
-class SharedAnnotation extends Annotation implements JsonSerializable{
+class SharedAnnotation extends Annotation implements JsonSerializable {
 
-	// TODO - this should be a list of LISGroup or LISPerson/s
-    public 	$withAgents =array();
+    // TODO - this should be a list of LISGroup or LISPerson/s
+    public $withAgents = array();
 
-	public function __construct($id){
-		parent::__construct($id);
-		$this->setType("http://purl.imsglobal.org/caliper/v1/SharedAnnotation");
-	}
+    public function __construct($id) {
+        parent::__construct($id);
+        $this->setType('http://purl.imsglobal.org/caliper/v1/SharedAnnotation');
+    }
 
-	/**
-	 * @return the users
-	 */
-	public function  getUsers() {
-		return $this->withAgents;
-	}
+    /**
+     * @param users
+     *            the users to set
+     */
+    public function setUsers($users) {
+        $this->withAgents = $users;
+        return $this;
+    }
 
-	/**
-	 * @param users
-	 *            the users to set
-	 */
-	public function  setUsers($users) {
-		$this->withAgents = $users;
-	}
-	public function jsonSerialize(){
-		return ['@id'=>$this->getId(),
-		'@type'=>$this->getType(),
-        'lastModifiedTime' => $this->getDateModified(),
-        'properties' => (object)$this->getExtensions(),
-		'target'=> $this->getTarget(),
-		'users'=>$this->getUsers(),
-		];
-	}
-	
+    public function jsonSerialize() {
+        return [
+            '@id' => $this->getId(),
+            '@type' => $this->getType(),
+            'lastModifiedTime' => $this->getDateModified(),
+            'properties' => (object) $this->getExtensions(),
+            'target' => $this->getTarget(),
+            'users' => $this->getUsers(),
+        ];
+    }
+
+    /**
+     * @return the users
+     */
+    public function getUsers() {
+        return $this->withAgents;
+    }
+
 }
