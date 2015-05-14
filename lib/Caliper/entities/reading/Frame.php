bfbe10ad690debc7bbeea6ca9bf877dbee77745e
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2015-05-14 11:18:30 -0400

    IMSGlobal/caliper-php#49 - Added new Frame class.

---
+++ lib/Caliper/entities/reading/Frame.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'Caliper/entities/CaliperDigitalResource.php';
+require_once 'Caliper/entities/CaliperEntityTypes.php';
+
+class Frame extends CaliperDigitalResource implements Targetable {
+    private $index;
+
+    public function __construct($id) {
+        parent::__construct($id);
+        $this->setType(CaliperEntityTypes::FRAME);
+    }
+
+    public function jsonSerialize() {
+        return array_merge(parent::jsonSerialize(), [
+            'index' => $this->getIndex(),
+        ]);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIndex() {
+        return $this->index;
+    }
+
+    /**
+     * @param mixed $index
+     * @return Frame
+     */
+    public function setIndex($index) {
+        $this->index = $index;
+        return $this;
+    }
+}
