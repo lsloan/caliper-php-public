acd6140c4d13ec00f75c7d1726d498a2d4997647
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2015-05-18 10:48:52 -0400

    IMSGlobal/caliper-php#61 - These classes no longer include roles.

--- lib/Caliper/entities/agent/SoftwareApplication.php
+++ lib/Caliper/entities/agent/SoftwareApplication.php
@@ -6,25 +6,8 @@ require_once 'Caliper/entities/foaf/Agent.php';
 require_once 'Caliper/entities/schemadotorg/SoftwareApplication.php';
 
 class SoftwareApplication extends Entity implements foaf\Agent, schemadotorg\SoftwareApplication {
-    private $roles = [];
-
     public function __construct($id) {
         parent::__construct($id);
         $this->setType(EntityType::SOFTWARE_APPLICATION);
-    }
-
-    public function jsonSerialize() {
-        return array_merge(parent::jsonSerialize(), [
-            'roles' => $this->getRoles(),
-        ]);
-    }
-
-    public function  getRoles() {
-        return $this->roles;
-    }
-
-    public function  setRoles($roles) {
-        $this->roles = $roles;
-        return $this;
     }
 }
