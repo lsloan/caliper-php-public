022f6122efca6a61e274bbeca1f5b90ba04741ae
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2015-05-19 17:30:46 -0400

    IMSGlobal/caliper-php#64 - Removal of a lot of test objects in order to use those created by new classes.

--- test/caliper/request/HttpRequestorTest.php
+++ test/caliper/request/HttpRequestorTest.php
@@ -1,91 +1,46 @@
 <?php
 require_once realpath(dirname(__FILE__) . '/../../../lib/CaliperSensor.php');
-require_once 'Caliper/entities/reading/EPubVolume.php';
 require_once 'Caliper/entities/reading/EPubSubChapter.php';
-require_once 'Caliper/entities/reading/Frame.php';
-require_once 'Caliper/entities/agent/Person.php';
 require_once 'Caliper/entities/lis/CourseSection.php';
 require_once 'Caliper/entities/lis/Group.php';
 require_once 'Caliper/entities/lis/Membership.php';
-require_once 'Caliper/entities/agent/SoftwareApplication.php';
 require_once 'Caliper/entities/DigitalResource.php';
-require_once 'Caliper/entities/agent/Organization.php';
 require_once 'Caliper/events/NavigationEvent.php';
-require_once 'Caliper/entities/reading/WebPage.php';
-require_once 'Caliper/entities/lis/Role.php';
 require_once realpath(CALIPER_LIB_PATH . '/../test/util/TestUtilities.php');
+require_once realpath(CALIPER_LIB_PATH . '/../test/caliper/TestRequests.php');
+require_once realpath(CALIPER_LIB_PATH . '/../test/caliper/TestTimes.php');
+require_once realpath(CALIPER_LIB_PATH . '/../test/caliper/TestAgentEntities.php');
+require_once realpath(CALIPER_LIB_PATH . '/../test/caliper/TestLisEntities.php');
+require_once realpath(CALIPER_LIB_PATH . '/../test/caliper/TestReadingEntities.php');
 
 class HttpRequestorTest extends PHPUnit_Framework_TestCase {
     private $testObject;
 
-    function  setUp() {
-        $createdTime = new DateTime('2015-08-01T06:00:00.000Z');
-        $modifiedTime = new DateTime('2015-09-02T11:30:00.000Z');
-
-        $navigationStartTime = new DateTime('2015-09-15T10:15:00.000Z');
-
-        $testPersonId = 'https://some-university.edu/user/554433';
-        $testRole = Role::LEARNER;
-
-        $courseOrganizationUrl = 'https://some-university.edu/politicalScience/2015/american-revolution-101';
+    function setUp() {
         $courseMembership = new Membership('https://some-university.edu/membership/001');
-        $courseMembership->setMember($testPersonId)
-            ->setOrganization($courseOrganizationUrl)
-            ->setRoles([$testRole])
-            ->setDateCreated($createdTime);
+        $courseMembership->setMember(TestAgentEntities::makePerson()->getId())
+            ->setOrganization(TestLisEntities::makeCourseOffering()->getId())
+            ->setRoles(TestLisEntities::makeMembership()->getRoles())
+            ->setDateCreated(TestTimes::createdTime());
 
         $sectionOrganizationUrl = 'https://some-university.edu/politicalScience/2015/american-revolution-101/section/001';
         $sectionMembership = new Membership('https://some-university.edu/membership/002');
-        $sectionMembership->setMember($testPersonId)
+        $sectionMembership->setMember(TestAgentEntities::makePerson()->getId())
             ->setOrganization($sectionOrganizationUrl)
-            ->setRoles([$testRole])
-            ->setDateCreated($createdTime);
+            ->setRoles(TestLisEntities::makeMembership()->getRoles())
+            ->setDateCreated(TestTimes::createdTime());
 
         $groupOrganizationUrl = 'https://some-university.edu/politicalScience/2015/american-revolution-101/section/001/group/001';
         $groupMembership = new Membership('https://some-university.edu/membership/003');
-        $groupMembership->setMember($testPersonId)
+        $groupMembership->setMember(TestAgentEntities::makePerson()->getId())
             ->setOrganization($groupOrganizationUrl)
-            ->setRoles([$testRole])
-            ->setDateCreated($createdTime);
+            ->setRoles(TestLisEntities::makeMembership()->getRoles())
+            ->setDateCreated(TestTimes::createdTime());
 
-        $testPerson = new Person($testPersonId);
-        $testPerson->setDateCreated($createdTime)
-            ->setDateModified($modifiedTime);
 
-        $membership = new Membership('https://some-university.edu/politicalScience/2015/american-revolution-101/roster/554433');
-        $membership
-            ->setDateCreated($createdTime)
-            ->setDescription('Roster entry')
-            ->setMember($testPersonId)
-            ->setName('American Revolution 101')
-            ->setOrganization('https://some-university.edu/politicalScience/2015/american-revolution-101/section/001')
-            ->setRoles($testRole);
 
-        $edApp = new SoftwareApplication('https://github.com/readium/readium-js-viewer');
-        $edApp->setName('Readium')
-            ->setDateCreated($createdTime)
-            ->setDateModified($modifiedTime);
 
-        $object = new EPubVolume('https://github.com/readium/readium-js-viewer/book/34843#epubcfi(/4/3)');
-        $object->setName('The Glorious Cause: The American Revolution, 1763-1789 (Oxford History of the United States)')
-            ->setDateCreated($createdTime)
-            ->setDateModified($modifiedTime)
-            ->setVersion('2nd ed.');
-
-        $target = new Frame('https://github.com/readium/readium-js-viewer/book/34843#epubcfi(/4/3/1)');
-        $target->setName('Key Figures: George Washington')
-            ->setDateCreated($createdTime)
-            ->setDateModified($modifiedTime)
-            ->setIsPartOf($object)
-            ->setVersion('2nd ed.')
-            ->setIndex(1);
-
-        $courseOffering = new CourseOffering($courseOrganizationUrl);
-        $courseOffering->setCourseNumber('POL101')
-            ->setName('Political Science 101: The American Revolution')
-            ->setAcademicSession('Fall-2015')
-            ->setDateCreated($createdTime)
-            ->setDateModified($modifiedTime);
+        $courseOffering = TestLisEntities::makeCourseOffering();
 
         $courseSection = new CourseSection($sectionOrganizationUrl);
         $courseSection->setCourseNumber('POL101')
@@ -93,38 +48,25 @@ class HttpRequestorTest extends PHPUnit_Framework_TestCase {
             ->setAcademicSession('Fall-2015')
             ->setMembership([$sectionMembership])
             ->setSubOrganizationOf($courseOffering)
-            ->setDateCreated($createdTime)
-            ->setDateModified($modifiedTime);
+            ->setDateCreated(TestTimes::createdTime())
+            ->setDateModified(TestTimes::modifiedTime());
 
         $group = new Group($groupOrganizationUrl);
         $group->setName('Discussion Group 001')
             ->setMembership([$groupMembership])
             ->setSubOrganizationOf($courseSection)
-            ->setDateCreated($createdTime);
+            ->setDateCreated(TestTimes::createdTime());
 
-        $fromResource = new WebPage('https://some-university.edu/politicalScience/2015/american-revolution-101/index.html');
-        $fromResource->setName('American Revolution 101 Landing Page')
-            ->setDateCreated($createdTime)
-            ->setDateModified($modifiedTime)
-            ->setVersion('1.0');
-
-        $navigationEvent = new NavigationEvent();
-        $navigationEvent->setActor($testPerson)
-            ->setMembership($membership)
-            ->setObject($object)
-            ->setNavigatedFrom($fromResource)
-            ->setEdApp($edApp)
-            ->setTarget($target)
+        $navigationEvent = (new NavigationEvent())->setActor(TestAgentEntities::makePerson())
+            ->setMembership(TestLisEntities::makeMembership())
+            ->setObject(TestReadingEntities::makeEPubVolume())
+            ->setNavigatedFrom(TestReadingEntities::makeWebPage())
+            ->setEdApp(TestAgentEntities::makeSoftwareApplication())
+            ->setTarget(TestReadingEntities::makeFrame())
             ->setGroup($group)
-            ->setStartedAtTime($navigationStartTime);
+            ->setStartedAtTime(TestTimes::navigationStartTime());
 
-        $envelope = new Envelope();
-        $envelope
-            ->setData($navigationEvent)
-            ->setSensor('http://learning-app.some-university.edu/sensor')
-            ->setSendTime(new DateTime('2015-09-15T11:05:01.000Z'));
-
-        $this->testObject = $envelope;
+        $this->testObject = TestRequests::makeEnvelope()->setData($navigationEvent);
     }
 
     /**
