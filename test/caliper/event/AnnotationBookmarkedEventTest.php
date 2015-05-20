31598440fbe1a31d945f0388e71ec455db2f2120
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2015-05-20 16:48:16 -0400

    IMSGlobal/caliper-php#66 - New test classes for annotations.

---
+++ test/caliper/event/AnnotationBookmarkedEventTest.php
@@ -0,0 +1,39 @@
+<?php
+require_once realpath(dirname(__FILE__) . '/../../../lib/CaliperSensor.php');
+require_once 'Caliper/actions/Action.php';
+require_once 'Caliper/events/AnnotationEvent.php';
+require_once realpath(CALIPER_LIB_PATH . '/../test/util/TestUtilities.php');
+require_once realpath(CALIPER_LIB_PATH . '/../test/caliper/TestAgentEntities.php');
+require_once realpath(CALIPER_LIB_PATH . '/../test/caliper/TestAnnotationEntities.php');
+require_once realpath(CALIPER_LIB_PATH . '/../test/caliper/TestLisEntities.php');
+require_once realpath(CALIPER_LIB_PATH . '/../test/caliper/TestReadingEntities.php');
+require_once realpath(CALIPER_LIB_PATH . '/../test/caliper/TestTimes.php');
+
+class AnnotationBookmarkedEventTest extends PHPUnit_Framework_TestCase {
+	private $testObject;
+
+	function setUp() {
+        $this->testObject = (new AnnotationEvent())
+            ->setActor(TestAgentEntities::makePerson())
+            ->setAction(Action::BOOKMARKED)
+            ->setObject(TestReadingEntities::makeFrame2())
+            ->setGenerated(TestAnnotationEntities::makeBookmarkAnnotation())
+            ->setStartedAtTime(TestTimes::startedTime())
+            ->setEdApp(TestAgentEntities::makeReadingApplication())
+            ->setGroup(TestLisEntities::makeGroup())
+            ->setMembership(TestLisEntities::makeMembership())
+        ;
+	}
+
+    /**
+     * @group passes
+     */
+	function testObjectSerializesToJson() {
+        $testJson = json_encode($this->testObject, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+		$testFixtureFilePath = realpath(CALIPER_LIB_PATH . '/../../caliper-common-fixtures/src/test/resources/fixtures/caliperBookmarkAnnotationEvent.json');
+
+        TestUtilities::saveFormattedFixtureAndOutputJson($testFixtureFilePath, $testJson, __CLASS__);
+
+		$this->assertJsonStringEqualsJsonFile($testFixtureFilePath, $testJson);
+	}
+}
