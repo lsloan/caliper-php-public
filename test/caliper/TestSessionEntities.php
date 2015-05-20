b1fddfc89affb1ad2f687d812bf982dbcbe44832
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2015-05-20 12:23:15 -0400

    IMSGlobal/caliper-php#64 - Additional entity classes for tests.

---
+++ test/caliper/TestSessionEntities.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'Caliper/entities/session/Session.php';
+
+class TestSessionEntities {
+    public static function makeSession() {
+        return (new Session('https://github.com/readium/session-123456789'))
+            ->setName('session-123456789')
+            ->setDateCreated(TestTimes::createdTime())
+            ->setDateModified(TestTimes::modifiedTime())
+            ->setActor(TestAgentEntities::makePerson())
+            ->setStartedAtTime(TestTimes::startedTime());
+    }
+}
