8706f09e574b47fef29bbfafb184b93c1cd86dfa
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2015-05-21 13:16:48 -0400

    IMSGlobal/caliper-php#69 - New test for OutcomeEvent graded.

---
+++ test/caliper/event/OutcomeGradedEventTest.php
@@ -0,0 +1,39 @@
+<?php
+require_once realpath(dirname(__FILE__) . '/../../../lib/CaliperSensor.php');
+require_once 'Caliper/actions/Action.php';
+require_once 'Caliper/events/OutcomeEvent.php';
+require_once realpath(CALIPER_LIB_PATH . '/../test/util/TestUtilities.php');
+require_once realpath(CALIPER_LIB_PATH . '/../test/caliper/TestAgentEntities.php');
+require_once realpath(CALIPER_LIB_PATH . '/../test/caliper/TestAssessmentEntities.php');
+require_once realpath(CALIPER_LIB_PATH . '/../test/caliper/TestAssignableEntities.php');
+require_once realpath(CALIPER_LIB_PATH . '/../test/caliper/TestLisEntities.php');
+require_once realpath(CALIPER_LIB_PATH . '/../test/caliper/TestTimes.php');
+
+class OutcomeGradedEventTest extends PHPUnit_Framework_TestCase {
+    private $testObject;
+
+    function setUp() {
+        $this->testObject = (new OutcomeEvent())
+            ->setActor(TestAgentEntities::makePerson())
+            ->setAction(Action::GRADED)
+            ->setObject(TestAssignableEntities::makeAttempt()
+                ->setAssignable(TestAssessmentEntities::makeAssessment()->getId()))
+            ->setGenerated(TestAssignableEntities::makeResult())
+            ->setStartedAtTime(TestTimes::startedTime())
+            ->setEdApp(TestAgentEntities::makeAssessmentApplication())
+            ->setGroup(TestLisEntities::makeGroup())
+            ->setMembership(TestLisEntities::makeMembership());
+    }
+
+    /**
+     * @group passes
+     */
+    function testObjectSerializesToJson() {
+        $testJson = json_encode($this->testObject, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+        $testFixtureFilePath = realpath(CALIPER_LIB_PATH . '/../../caliper-common-fixtures/src/test/resources/fixtures/caliperAssessmentOutcomeEvent.json');
+
+        TestUtilities::saveFormattedFixtureAndOutputJson($testFixtureFilePath, $testJson, __CLASS__);
+
+        $this->assertJsonStringEqualsJsonFile($testFixtureFilePath, $testJson);
+    }
+}
