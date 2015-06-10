a2726949f8b536197eabcd17f70a1fa35259f5ae
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2015-06-10 16:24:09 -0400

    IMSGlobal/caliper-php#92 - New class to provide support for OOP shortcomings in some versions of PHP.  Specifically, getting the name of a class without an instance.

---
+++ lib/Caliper/util/ClassUtil.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Class ClassUtil
+ *
+ * Provide useful methods to overcome OOP shortcomings in some versions of PHP.
+ */
+class ClassUtil {
+    /**
+     * The "::class" notation isn't available until PHP 5.5.  This method is a workaround for
+     * older versions of PHP.
+     *  
+     * @return string Name of this class
+     */
+    static public function className() {
+        return get_called_class();
+    }
+}
