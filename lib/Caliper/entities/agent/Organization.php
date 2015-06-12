a7be293f48f296f380dad4ecdefae49596005f3c
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2015-06-12 16:30:04 -0400

    IMSGlobal/caliper-php#96 - Corrections/simplifications.

--- lib/Caliper/entities/agent/Organization.php
+++ lib/Caliper/entities/agent/Organization.php
@@ -1,39 +1,28 @@
 <?php
-require_once 'Sensor.php';
 require_once 'Caliper/entities/Entity.php';
 require_once 'Caliper/entities/foaf/Agent.php';
+require_once 'Caliper/entities/w3c/Organization.php';
 
-class Organization extends Entity implements foaf\Agent, w3c\Organization {
-    /** @var w3c\Organization */
-    private $parentOrg;
+class Organization extends Entity implements \foaf\Agent, \w3c\Organization {
+    /** @var \w3c\Organization */
+    private $subOrganizationOf;
 
     public function __construct($id) {
         parent::__construct($id);
         $this->setType(new EntityType(EntityType::ORGANIZATION));
     }
 
-    public function jsonSerialize() {
-        $serializedParent = parent::jsonSerialize();
-
-        // This class doesn't use these properties, so unset them.
-        unset($serializedParent['name']);
-        unset($serializedParent['description']);
-
-        return $serializedParent;
-    }
-
-    /** @return w3c\Organization */
-    public function  getParentOrg() {
-        return $this->parentOrg;
+    /** @return \w3c\Organization */
+    public function  getSubOrganizationOf() {
+        return $this->subOrganizationOf;
     }
 
     /**
-     * @param w3c\Organization $parentOrg
+     * @param \w3c\Organization $subOrganizationOf
      * @return $this|Organization
      */
-    public function  setParentOrg(w3c\Organization $parentOrg) {
-        $this->parentOrg = $parentOrg;
+    public function  setSubOrganizationOf(\w3c\Organization $subOrganizationOf) {
+        $this->subOrganizationOf = $subOrganizationOf;
         return $this;
     }
-
 }
